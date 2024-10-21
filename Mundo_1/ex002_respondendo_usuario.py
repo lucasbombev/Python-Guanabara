f9bcab5175bdb9d0d511b916e9370678146f9486
Lucas PC <olucasbm1213@gmail.com>
2024-10-21 00:05:13 -0300

    ex002 respondendo usuario

---
+++ Mundo_1/ex002_respondendo_usuario.py
@@ -0,0 +1,7 @@
+#Exercício Python 002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+nome = input("Digite seu nome:")
+if 'Lucas' in nome:
+    print("Que nome lindo!")
+    print(f"Seja bem vindo, {nome}!")
+else:
+    print(f"Seja bem vindo, {nome}!")

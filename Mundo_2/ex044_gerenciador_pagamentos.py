9d5b7346777ca399b2057e3f7854d98d490b80ac
thinkpad-Lucas <olucasbm1213@gmail.com>
2024-12-05 08:07:23 -0300

    ex044 to complete

---
+++ Mundo_2/ex044_gerenciador_pagamentos.py
@@ -0,0 +1,16 @@
+'''Exercício Python 044: Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:4
+à vista dinheiro/cheque: 10% de desconto
+à vista no cartão: 5% de desconto
+em até 2x no cartão: preço formal
+3x ou mais no cartão: 20% de juros'''
+compras = float(input("Preço das compras: R$:"))
+preco_avista =
+preco_vencimento = 
+preco_2x = 
+preco_3x = 
+escolha_pagamento = int(input(('''   FORMAS DE PAGAMENTO
+      [ 1 ] à vista
+      [ 2 ] vencimento
+      [ 3 ] 2x crédito
+      [ 4 ] 3x ou mais crédito
+      ''')))

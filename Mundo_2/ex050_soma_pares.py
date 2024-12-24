56742e3d8a43bd72b90e7614fe1daaefaa98cb5c
Lucas ThinkPad <olucasbm1213@gmail.com>
2024-12-24 09:28:31 -0300

    up to date

--- Mundo_2/ex050_soma_pares.py
+++ Mundo_2/ex050_soma_pares.py
@@ -1,4 +1,4 @@
-#Exercício Python 050: Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+#Exercício Python 050: Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o!
 soma = 0
 for c in range(1,7):
     numero = int(input("Digite um número: "))

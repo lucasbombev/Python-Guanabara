15228df65374a07a9c1a3d5ca9a13194a5317cf4
Lucas PC PopOS! <olucasbm1213@gmail.com>
2025-03-28 11:21:05 -0300

    code comment

--- Mundo_2/ex068_par_impar.py
+++ Mundo_2/ex068_par_impar.py
@@ -1,22 +1,27 @@
 #Exercício Python 068: Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador perder, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo. 
 import random
+# Imprime a introdução do jogo
 print("=-"*10) 
 print("JOGO DO PAR OU IMPAR")
 print("=-"*10) 
-
+# Gera um número aleatório entre 1 e 10 para o computador
 computador = random.randint(1, 10)
+# Inicializa as variáveis
 calcula_par_impar = 0
 conta_vitorias = 0
-
+# Loop principal do jogo
 while True:
+    # Recebe o valor do jogador
     player_number = int(input("Diga um valor:  "))
+    # Recebe a escolha do jogador (Par ou Ímpar) e converte para maiúsculas
     player_option = input("Par ou Ímpar? [P/I] ").upper()
+    # Calcula se a soma é par ou ímpar
     calcula_par_impar = (player_number + computador) % 2
+    # Verifica se o jogador escolheu Par e a soma é ímpar, então o jogo termina
     if player_option == 'P' and calcula_par_impar != 0:
         break
+    # Incrementa o contador de vitórias
     conta_vitorias += 1
+    # Imprime o resultado da rodada
     print('-'*10)
-    print(f"você jogou {player_number} e o computador {computador}. Total de {player_number + computador}")
-    print('-'*10)
-print("=-"*10)    
-print(f"GAME OVER! você venceu {conta_vitorias} vezes.")
+    print(f"Você jogou {player_number} e o computador {computador 
